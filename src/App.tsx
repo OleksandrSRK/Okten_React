21ef7a2e8e27ffa40bf27e028aa4c491082c0bde
OleksandrSRK <oleksandr2005srk@gmail.com>
2026-07-24 18:04:24 +0300

    UseState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import './App.css'
+import {useState} from 'react'
 
 function App() {
 
+    // eslint-disable-next-line prefer-const
+    let [counter, setCounter] = useState<number>(0);
   return (
     <>
+        <div>
+            <h2>{counter}</h2>
+            <button onClick={()=>{
+                console.log('+ ' + counter);
+                setCounter(++counter);
+            }}>
+                increment
+            </button>
 
+            <button onClick={()=>{
+                console.log('- ' + counter);
+                setCounter(prevState => {
+                    return prevState - 1;
+                });
+            }}>
+                decrement
+            </button>
+        </div>
     </>
   )
 }
