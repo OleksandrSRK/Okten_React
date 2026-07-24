8c55349c1b61e4b00b0be33d774c5bed6079f375
OleksandrSRK <oleksandr2005srk@gmail.com>
2026-07-24 19:21:18 +0300

    Service layer

--- src/components/users-component/UsersComponent.tsx
+++ src/components/users-component/UsersComponent.tsx
@@ -1,14 +1,14 @@
 import {useEffect, useState} from 'react';
 import type {IUser} from "../../models/IUser.ts";
 import UserComponent from "../user-component/UserComponent.tsx";
+import {getUsers} from "../../services/api.service.ts";
 
 const UsersComponent = () => {
 
     const [users, setUsers] = useState<IUser[]>([])
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(value => value.json())
+        getUsers()
             .then(response => {
                 setUsers(response);
             });
