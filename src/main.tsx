3d292168cdc08f78f94fcd3d3a11fbbf5aea1483
OleksandrSRK <oleksandr2005srk@gmail.com>
2026-08-06 19:26:43 +0300

    Task #3.1 part 2 is done

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,30 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomePages from "./pages/HomePages.tsx";
+
+createRoot(document.getElementById('root')!)
+    .render(<BrowserRouter>
+        <Routes>
+            <Route path={'/'} element={<HomePages/>}>
+                <Route path={'users'} element={<div>Users</div>}/>
+                <Route path={'posts'} element={<div>Posts</div>}/>
+                <Route path={'comments'} element={<div>Comments</div>}/>
+                <Route path={'products'} element={<div>Products</div>}/>
+            </Route>
+        </Routes>
+    </BrowserRouter>);
+
+
+
+
+
+/*
+import { createRoot } from 'react-dom/client'
+import './index.css'
 import {RouterProvider} from "react-router-dom";
 import {router} from "./router/routes.tsx";
 
 createRoot(document.getElementById('root')!)
     .render(<RouterProvider router={router}/>);
+*/
