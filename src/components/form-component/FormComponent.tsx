1118b1cfeaaf69806cf06aa34d7b6aedb7f216a3
OleksandrSRK <oleksandr2005srk@gmail.com>
2026-09-17 04:41:18 +0300

    Forms uncontrolled #3

--- src/components/form-component/FormComponent.tsx
+++ src/components/form-component/FormComponent.tsx
@@ -1,15 +1,14 @@
-import {type FormEvent, type SubmitEvent, useState} from "react";
-
+import {useForm} from "react-hook-form";
 
 interface IFormProps {
     username: string,
     password: string,
-
+    age: number,
 }
 
 
 const FormComponent = () => {
-
+/*
     const [formState, setFormState] = useState<IFormProps>({
         username: 'user1',
         password: '1234'
@@ -23,7 +22,7 @@ const FormComponent = () => {
         };
         console.log(user);
     };
-/*
+/////
     const handleUsernameChange = (e:FormEvent<HTMLInputElement>) => {
         const input = e.target as HTMLInputElement;
         console.log(input.value);
@@ -36,7 +35,7 @@ const FormComponent = () => {
         console.log(input.name);
         setFormState({...formState, password: input.value});
     }
-*/
+/////
 
     const handleInputChange = (e:FormEvent<HTMLInputElement>) => {
         const input = e.target as HTMLInputElement
@@ -55,4 +54,53 @@ const FormComponent = () => {
     );
 };
 
+*/
+
+    const {
+        handleSubmit,
+        register,
+        formState: {errors, isValid}
+    } = useForm<IFormProps>({
+        mode:'all'
+    })
+
+    const customHandler = (formDataProps: IFormProps)=>{
+        console.log(formDataProps)
+    }
+
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit(customHandler)}>
+                <label>
+                    <input type="text" {...register('username', {
+                        required: true,
+                        minLength: {value:1, message: 'name too short'},
+                        maxLength: {value:16, message: 'name too long'}
+                    })}/>
+                    {errors.username && <div>{errors.username.message}</div>}
+                </label>
+
+                <label>
+                    <input type="text" {...register('password', {
+                        required: true,
+                        minLength: {value:3, message: 'pass too short'},
+                        maxLength: {value:8, message: 'pass too long'}
+                    })}/>
+                    {errors.password && <div>{errors.password.message}</div>}
+                </label>
+
+                <label>
+                    <input type="number" {...register('age', {
+                        required: true,
+                        min: {value:1, message: 'age too small'},
+                        max: {value:117, message: 'age too big'}
+                    })}/>
+                    {errors.age && <div>{errors.age.message}</div>}
+                </label>
+                <button disabled={!isValid}>Sent</button>
+            </form>
+        </div>
+    );
+};
 export default FormComponent;
